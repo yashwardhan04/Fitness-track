7dcf1fed68130a9b9f4c2cfe2d5c93309f27a141
Yashwardhan Srivastava <yashwardhansrivastava14@gmail.com>
2025-08-02 21:43:43 +0530

    Added tutorial links

--- client/src/utils/tutorialsData.js
+++ client/src/utils/tutorialsData.js
@@ -8,52 +8,103 @@ const tutorials = [
   },
   {
     id: 2,
-    title: "Pull-Up",
-    description: "A basic back exercise.",
+    title: "Bench Press",
+    description: "A compound chest exercise.",
     videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Back"
+    muscleGroup: "Chest"
   },
   {
     id: 3,
-    title: "Bicep Curl",
-    description: "A basic arms exercise.",
+    title: "Chest Fly",
+    description: "Fly movement for chest.",
     videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Arms"
+    muscleGroup: "Chest"
   },
   {
     id: 4,
-    title: "Shoulder Press",
-    description: "A basic shoulder exercise.",
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Shoulders"
+    title: "Pull-Up",
+    description: "A basic back exercise.",
+    videoUrl: "https://youtube.com/embed/aRFqDO7XClU",
+    muscleGroup: "Back"
   },
+
+  
   {
     id: 5,
-    title: "Crunches",
-    description: "A basic abs exercise.",
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Abs"
+    title: "Bicep Curl",
+    description: "A basic arms exercise.",
+    videoUrl: "https://youtube.com/embed/803JIAWBj_c",
+    muscleGroup: "Arms"
   },
   {
     id: 6,
-    title: "Squat",
-    description: "A basic legs exercise.",
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Legs"
+    title: "Shoulder Press",
+    description: "A basic shoulder exercise.",
+    videoUrl: "https://youtube.com/embed/k6tzKisR3NY?",
+    muscleGroup: "Shoulders"
   },
   {
     id: 7,
-    title: "Neck Flexion",
-    description: "A basic neck exercise.",
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    muscleGroup: "Neck"
+    title: "Crunches",
+    description: "A basic abs exercise.",
+    videoUrl: "https://youtube.com/embed/eeJ_CYqSoT4",
+    muscleGroup: "Abs"
   },
   {
     id: 8,
+    title: "Squat",
+    description: "A basic legs exercise.",
+    videoUrl: "https://youtube.com/embed/MLoZuAkIyZI",
+    muscleGroup: "Legs"
+  },
+  {
+    id: 9,
+    title: "Neck Flexion",
+    description: "A basic neck exercise.",
+    videoUrl: "https://youtube.com/embed/lRKWP2LzCmw",
+    muscleGroup: "Neck"
+  },
+  {
+    id: 10,
     title: "Recommended Exercise",
     description: "A recommended exercise for all.",
     videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
     muscleGroup: "Recommended"
+  },
+  {
+    id: 11,
+    title: "Deadlift",
+    description: "A recommended exercise for lower back",
+    videoUrl: "https://youtube.com/embed/vfKwjT5-86k",
+    muscleGroup: "Recommended"
+  },
+  {
+    id: 12,
+    title: "Machine Rows",
+    description: "A recommended exercise for back.",
+    videoUrl: "https://youtube.com/embed/Y_F7NlVoJqY",
+    muscleGroup: "Recommended"
+  },
+  {
+    id: 13,
+    title: "Lateral Raises",
+    description: "Lateral raises for shoulders.",
+    videoUrl: "https://youtube.com/embed/Kl3LEzQ5Zqs",
+    muscleGroup: "Recommended"
+  },
+  {
+    id: 14,
+    title: "Leg Curls",
+    description: "A recommended exercise for hamstrings.",
+    videoUrl: "https://youtube.com/embed/_lgE0gPvbik",
+    muscleGroup: "Legs"
+  },
+  {
+    id: 15,
+    title: "Recommended Exercise",
+    description: "A recommended exercise for all.",
+    videoUrl: "https://youtube.com/embed/b_r_LW4HEcM",
+    muscleGroup: "Arms"
   }
 ];
 
